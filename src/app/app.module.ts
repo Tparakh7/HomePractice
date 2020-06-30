1f3337cafc00f3237a910e345fc3e953660f42f7
tparakh7 <tparakh7@gmail.com>
2020-06-30 16:46:26 +0530

    material-fail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { ServerssComponent } from './serverss/serverss.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatDatepickerModule, MatDatepicker} from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { ServerssComponent } from './serverss/serverss.component';
     ServerssComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
